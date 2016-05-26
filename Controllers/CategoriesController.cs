38d77efd6b0b636b8df0e45d410a0b3f8f8b0a5b
Haiderali524 <bsef12m524@pucit.edu.pk>
2016-05-26 22:58:03 +0500

    push new code

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using aghaApi.Models;
+
+namespace aghaApi.Controllers
+{
+    public class categoriesController : ApiController
+    {
+        private finalEntities db = new finalEntities();
+
+        // GET api/categories
+       public IQueryable<category> Get()
+        {
+            return db.categories;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(string))]
+       public IHttpActionResult verify(string username, string pass)
+       {
+           List<user> list = db.users.ToList();
+            foreach(user u in list)
+            {
+                if(u.username.Equals(username) && u.password.Equals(pass))
+                {
+                    return Ok("Success");
+                }
+            }
+           return Ok("failure");
+       }
+
+        // GET api/categories/5
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Getcategory(int id)
+        {
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        // PUT api/categories/5
+        public IHttpActionResult Putcategory(int id, category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!categoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/categories
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Postcategory(category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.categories.Add(category);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = category.Id }, category);
+        }
+
+        // DELETE api/categories/5
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Deletecategory(int id)
+        {
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.categories.Remove(category);
+            db.SaveChanges();
+
+            return Ok(category);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool categoryExists(int id)
+        {
+            return db.categories.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,21 @@
+﻿using aghaApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace aghaApi.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        finalEntities db = new finalEntities();
+
+        public object GetServices()
+        {
+            return db.services;
+        }
+
+    }
+}
