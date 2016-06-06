04d64606a89d640402c160012e25fb23048f6791
Haiderali524 <bsef12m524@pucit.edu.pk>
2016-06-06 21:58:23 +0500

    nisar
    haider

---
+++ aghaApi/aghaApi/Controllers/CategoriesController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using aghaApi.Models;
+
+namespace aghaApi.Controllers
+{
+    public class categoriesController : ApiController
+    {
+        private finalEntities db = new finalEntities();
+
+        // GET api/categories
+       public IQueryable<category> Get()
+        {
+            return db.categories;
+        }
+       [HttpGet]
+       [ResponseType(typeof(string))]
+       public IHttpActionResult requestGrocery(int uid, string items)
+       {
+           Grocery grocery = new Grocery();
+           grocery.uid = uid;
+           grocery.items = items;
+           grocery.date = DateTime.Now;
+           try
+           {
+               db.Groceries.Add(grocery);
+               db.SaveChanges();
+               return Ok("success");
+           }
+           catch (Exception e)
+           {
+
+           }
+           return Ok("failure");
+       }
+
+        [HttpGet]
+        [ResponseType(typeof(worker_Portfolio))]
+        public IHttpActionResult getAvailableWorkers(int sid, string slot)
+       {
+           List<Availability_Slots> list = db.Availability_Slots.Where(x=> x.Availability_Slots1.Equals(slot) && x.IsAvailable == 0).ToList();
+           List<worker_Portfolio> li = new List<worker_Portfolio>();
+            foreach(Availability_Slots slots in list)
+            {
+                if(slots.worker_Portfolio.sid==sid)
+                {
+                    li.Add(slots.worker_Portfolio);
+          
+                }
+            }
+           return Ok(li);
+       }
+        [HttpGet]
+        [ResponseType(typeof(string))]
+       public IHttpActionResult verify(string username, string pass)
+       {
+           List<user> list = db.users.ToList();
+            foreach(user u in list)
+            {
+                if(u.username.Equals(username) && u.password.Equals(pass))
+                {
+                    return Ok("Success");
+                }
+            }
+           return Ok("failure");
+       }
+        [HttpGet]
+        [ResponseType(typeof(List<request>))]
+        public IHttpActionResult getRequests(int uid)
+        {
+            List<request> li = db.requests.Where(x => x.user_id == uid).ToList();
+            return Ok(li);
+        }
+        [HttpGet]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult sendRequest(int wid, int sid, int uid, string slot)
+        {
+            request Request = new request();
+            Request.requested_worker_id = wid;
+            Request.sid = sid;
+            Request.user_id = uid;
+            Request.request_status = "Pending";
+            Request.request_date = DateTime.Now;
+            Request.request_time = DateTime.Now.TimeOfDay;
+            Request.Alloted_slots = slot;
+            try
+            {
+                db.requests.Add(Request);
+                List<Availability_Slots> slots = db.Availability_Slots.ToList();
+               foreach (Availability_Slots s in slots)
+               {
+                   if(s.Availability_Slots1.Equals(slot) && s.wid==wid)
+                   {
+                       s.IsAvailable = 1;
+                       db.SaveChanges();
+                       return Ok("success");
+                   }
+               }
+               
+                
+            }
+            catch(Exception e)
+            {
+
+            }
+
+
+            
+            return Ok("failure");
+        }
+
+
+        // GET api/categories/5
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Getcategory(int id)
+        {
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        // PUT api/categories/5
+        public IHttpActionResult Putcategory(int id, category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!categoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/categories
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Postcategory(category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.categories.Add(category);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = category.Id }, category);
+        }
+
+        // DELETE api/categories/5
+        [ResponseType(typeof(category))]
+        public IHttpActionResult Deletecategory(int id)
+        {
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.categories.Remove(category);
+            db.SaveChanges();
+
+            return Ok(category);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool categoryExists(int id)
+        {
+            return db.categories.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ aghaApi/aghaApi/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Web.UI;
+using aghaApi.Models;
+using System.Net;
+
+namespace aghaApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        finalEntities ctx = new finalEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Object aboutUs()
+        {
+            return View();
+        }
+
+
+        public Object sendMail(String from, String name, String subject, String message)
+        {
+            MailMessage mail = new MailMessage();
+
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com",587);
+            smtpServer.Credentials = new System.Net.NetworkCredential("serviceontime12@gmail.com", "03237537053");
+            smtpServer.EnableSsl = true;
+           //mail.From = new MailAddress(from);
+            mail.From = new MailAddress("serviceontime12@gmail.com");
+            mail.To.Add("serviceontime2@gmail.com");
+            mail.Subject = subject;
+            mail.Body = message + "\n" + from;
+
+            smtpServer.Send(mail);
+
+            return RedirectToAction("index", "Home");
+        }
+
+
+        [HttpPost]
+        public ActionResult signUp2(String name, String cnic, String address, String username, String contact)
+        {
+            user_request ur = new user_request();
+            ur.name = name;
+            ur.cnic = Convert.ToDecimal(cnic.Replace("-", ""));
+            ur.address = address;
+            ur.username = username;
+            ur.contact = Convert.ToDecimal(contact.Replace("-", ""));
+
+            try
+            {
+                finalEntities dben = new finalEntities();
+
+                foreach (user u in dben.users)
+                {
+                    if (u.username.Equals(ur.username))
+                    {
+                        TempData["error"] = "Error";
+                        return RedirectToAction("Signup", "Home");
+
+                    }
+                }
+
+                dben.user_request.Add(ur);
+                dben.SaveChanges();
+
+                for (int i = 0; i < Request.Files.Count; i++)
+                {
+                    HttpPostedFileBase file = Request.Files[i];
+                    file.SaveAs(Server.MapPath(@"~\images\user_request\user_" + ur.Id + ".jpeg"));
+                    break;
+                }
+                TempData["error"] = "Error";
+                Response.Write(@"<script language='javascript'>alert('The following errors have occurred:  .');</script>");
+                // Response.Write("<script>alert('Data inserted successfully')</script>");
+            }
+            catch (Exception e)
+            {
+                Exception e2 = e.InnerException;
+            }
+            return RedirectToAction("Index", "Home");
+
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        public ActionResult AllServices()
+        {
+            List<service> list = ctx.services.ToList();
+            return View(list);
+        }
+    }
+}
